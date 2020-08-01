df7facd989f7d695100ac02662bd4b07b743ba7d
PilYeong <pilyeooong@gmail.com>
2020-08-01 11:26:04 +0900

    ADD: passport 활용 유저 인증 추가

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const passport = require('passport');
+const passportConfig = require('./passport');
 const db = require('./models');
 
 const apiRouter = require('./routes');
@@ -17,21 +20,28 @@ db.sequelize
   })
   .catch((err) => {
     console.error(err);
-  }) 
+  });
 
+passportConfig();
+
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
-  saveUninitialized: false,
-  resave: false,
-  secret: process.env.COOKIE_SECRET
-}));
+app.use(
+  session({
+    saveUninitialized: false,
+    resave: false,
+    secret: process.env.COOKIE_SECRET,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/api', apiRouter);
 
-app.listen(3000, () => {
-  console.log('server is running on port 3000');
-})
+app.listen(4000, () => {
+  console.log('server is running on port 4000');
+});
 
 module.exports = app;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,56 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const { User } = require('../models');
 
 const router = express.Router();
 
-
 router.get('/', (req, res, next) => {
   return res.status(200).send('GET api users...');
-})
+});
+
+router.post('/login', async (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      console.error(err);
+      return next(err);
+    }
+    if (info) {
+      return res.status(401).send(info.reason);
+    }
+    return req.login(user, async (loginErr) => {
+      if (loginErr) {
+        console.error(loginErr);
+        return next(loginErr);
+      }
+      const loginUser = await User.findOne({
+        where: { id: user.id },
+        attributes: {
+          exclude: ['password'],
+        },
+      });
+      return res.status(200).send(loginUser);
+    });
+  })(req, res, next);
+});
+
+router.post('/', async (req, res, next) => {
+  const exUser = await User.findOne({ where: { email: req.body.email } });
+  if (exUser) {
+    return res.status(409).send('이미 사용중인 이메일입니다.');
+  }
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  try {
+    const user = await User.create({
+      email: req.body.email,
+      nickname: req.body.nickname,
+      password: hashedPassword,
+    });
+    return res.status(201).send('회원가입 성공');
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
 
 module.exports = router;
