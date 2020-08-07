fdbec5189dd7cb9607bed2c14ce27c7689410120
PilYeong <pilyeooong@gmail.com>
2020-08-07 15:30:13 +0900

    API: 출발지, 목적지 좌표 반환 및 동선 표시를 위한 데이터 요청 코드

--- backend/routes/place.js
+++ backend/routes/place.js
@@ -5,38 +5,59 @@ const { response } = require('express');
 
 const router = express.Router();
 
+const headers = {
+  'X-NCP-APIGW-API-KEY-ID': `${process.env.NAVER_MAP_CLIENT}`,
+  'X-NCP-APIGW-API-KEY': `${process.env.NAVER_MAP_CLIENT_SECRET}`,
+};
+
 router.post('/directions', async (req, res, next) => {
   const {
     data: { origin, destination },
   } = req.body;
-  console.log(origin, destination);
-  var headers = {
-    'X-NCP-APIGW-API-KEY-ID': `${process.env.NAVER_MAP_CLIENT}`,
-    'X-NCP-APIGW-API-KEY': `${process.env.NAVER_MAP_CLIENT_SECRET}`,
+
+  const startPoint = `${parseFloat(origin.lat)},${parseFloat(origin.lng)}`;
+  const endPoint = (`${parseFloat(destination.lat)},${parseFloat(destination.lng)}`);
+
+  console.log(startPoint);
+
+  const options = {
+    url: `https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving?start=${startPoint}&goal=${endPoint}?option=trafast`,
+    headers,
   };
 
-  // const origin = '127.1058342,37.359708';
-  // const destination = '129.075986,35.179470';
-
-  var options = {
-    url: `https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving?start=${origin}&goal=${destination}`,
-    headers: headers,
-  };
   try {
-    // await request(options, (err, response, body) => {
-    //   if(err) {
-    //     console.error(err);
-    //     next(err);
-    //   }
-    //   return res.status(200).send(response.body);
-    // });
+    await request(options, (err, response, body) => {
+      if (err) {
+        console.error(err);
+        next(err);
+      }
+      return res.status(200).send(response.body);
+    });
   } catch (err) {
     console.error(err);
     next(err);
   }
-
-  return res.status(200).send(origin, destination);
 });
 
-router.get('/geocode', async (req, res, next) => {});
+router.get('/geocode/:place', async (req, res, next) => {
+  const options = {
+    url: `https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=${encodeURI(
+      req.params.place
+    )}`,
+    headers,
+  };
+
+  try {
+    await request(options, (err, response, body) => {
+      if (err) {
+        console.err(err);
+        next(err);
+      }
+      return res.status(200).send(response.body);
+    });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
 module.exports = router;
